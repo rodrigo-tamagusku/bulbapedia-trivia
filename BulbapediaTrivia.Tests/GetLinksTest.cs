faadaa87aa210372fc9d0fc8f56127b34b1325c6
Rodrigo Minoru Tamagusku <rodrigo.tamagusku@gmail.com>
2026-05-11 13:27:43 -0300

    Create project, use a static dictionary to iterate over the 1025 pokemons, use the basic link template for the bulbapedia archives website

---
+++ BulbapediaTrivia.Tests/GetLinksTest.cs
@@ -0,0 +1,15 @@
+﻿namespace BulbapediaTrivia.Tests
+{
+    public class GetLinksTest
+    {
+        [Test]
+        public void GetAllImageLinks()
+        {
+            Dictionary<int, string> linksPokedex = GetLinks.GetAllImageLinks();
+            foreach (var item in linksPokedex)
+            {
+                Console.WriteLine($"ID: {item.Key}, Name: {item.Value}");
+            }
+        }
+    }
+}
---
+++ BulbapediaTrivia/GetLinks.cs
@@ -0,0 +1,25 @@
+﻿namespace BulbapediaTrivia
+{
+    public static class GetLinks
+    {
+        private static readonly int MAX_POKEDEX_SIZE = 1025;
+
+        public static Dictionary<int, string> GetAllImageLinks()
+        {
+            var dict = new Dictionary<int, string> { };
+
+            for (int i = 0; i <= MAX_POKEDEX_SIZE; i++)
+            {
+                string link = GetIconeFromPokedexNumber(i);
+                dict.Add(i, link);
+            }
+            return dict;
+        }
+
+        private static string GetIconeFromPokedexNumber(int i)
+        {
+            string numberXXXX = i.ToString("0000");
+            return $"https://archives.bulbagarden.net/wiki/File:Menu_HOME_{numberXXXX}.png";
+        }
+    }
+}
